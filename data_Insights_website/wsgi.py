e148398908f88631c16aa68ba14c31eec4333272
AsnelChristian <ngoullasob@gmail.com>
2018-06-05 20:03:07 +0100

    Coding style
    Define a .coafile and .jsbeautifyrc files.

--- data_Insights_website/wsgi.py
+++ data_Insights_website/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "data_Insights_website.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'data_Insights_website.settings')
 
 application = get_wsgi_application()
