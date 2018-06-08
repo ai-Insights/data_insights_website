ec0291cac47560d31744157d1cc8291b89042253
Kefeh Collins <kefehcollins@gmail.com>
2018-06-08 18:14:32 +0100

    Added the docs&help page amd added yet to be styled code for the visualisation tab
    Added the styling for the main card named it card-lg by extending card in _card.scss
    
    Added some few modifications and beautified the css file
    
    Added a _lists&dropdown.scss file and included some incons from font awsome
    
    added some formatting for a container and padding for the large card header
    
    added formatting for the texts elements
    
    Imported the _list&dropdown.scss file to the main scss file
    
    added some tab navigation controls with javascript
    
    Added the dommy stats and visual sub tabs
    
    Making active button to always be filled in _button.scss file
    
    Making the active button for the page filled doc&help.html file and adding the sticky-top class to it
    
    Making the navbar to stick to top on scroll home.html
    
    Add the container style to the _row.scss file
    
    Using the css without beautify
    
    added formatting for the texts elements
    
    Imported the _list&dropdown.scss file to the main scss file

---
+++ data_Insights_website/static/js/tabs.js
@@ -0,0 +1,17 @@
+function openNow(evt, idOfType) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("card-body");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(idOfType).style.display = "block";
+    evt.currentTarget.className += " active";
+}
