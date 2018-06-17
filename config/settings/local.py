806a6f66cb8b89805dbbd2950658f067ad2b382d
Asnel Christian <ngoullasob@gmail.com>
2018-06-17 10:43:10 +0100

    Define dev settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,101 +1,27 @@
-import environ
+from .base import *
+from .base import env
 
-ROOT_DIR = environ.Path(__file__) - 3  # (data_insights_website/config/settings/settings.py)
-APPS_DIR = ROOT_DIR.path('data_Insights_website')
-
-# TODO: create base and production settings files
-# SECURITY WARNING: don't run with debug turned on in production!
-env = environ.Env(DEBUG=(bool, True))
-environ.Env.read_env(str(ROOT_DIR.path('.env')))
-
+DEBUG = True
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
 
-ALLOWED_HOSTS = []
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
-DATABASES = {
-    'default': env.db()
+# https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
+
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
 }
 
-ROOT_URLCONF = 'config.urls'
-WSGI_APPLICATION = 'config.wsgi.application'
-
-# Application definition
-
-DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-THIRD_PARTY_APPS = []
-LOCAL_APPS = [
-    'data_Insights_website.app',
-    'data_Insights_website.docs'
-]
-
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
-
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [
-        str(APPS_DIR.path('templates'))
-    ],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-    },
-}, ]
-
-MEDIA_ROOT = str(APPS_DIR('media'))
-MEDIA_URL = '/media/'
-STATIC_ROOT = str(APPS_DIR('staticfiles'))
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
-]
-
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+INSTALLED_APPS += ['django_extensions']
