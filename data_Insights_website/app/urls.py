21a346096504fb4a5f2f4f441742ebc04cdf3029
Asnel Christian <ngoullasob@gmail.com>
2018-06-17 18:02:50 +0100

    Define Some app views

--- data_Insights_website/app/urls.py
+++ data_Insights_website/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.collect_data, name='colect_data')
+    path('', views.collect_data, name='collect_data'),
+    path('success', views.loaded_data, name='loaded_data')
 ]
