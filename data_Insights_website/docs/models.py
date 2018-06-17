8084178143bc413b5b20bec3cdacbda602b9f77c
Asnel Christian <ngoullasob@gmail.com>
2018-06-17 03:29:07 +0100

    Define docs models

--- data_Insights_website/docs/models.py
+++ data_Insights_website/docs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class DocumentationGroup(models.Model):
+    title = models.CharField(max_length=50)
+
+class DocumentationSubGroup(models.Model):
+    title = models.CharField(max_length=50)
+    short_description = models.TextField()
+    parent_group = models.ForeignKey(DocumentationGroup, on_delete=models.CASCADE)
+
+
+class DocumentationEntry(models.Model):
+    title = models.CharField(max_length=50)
+    icon = models.ImageField(blank=True)
+    description = models.TextField()
+    image = models.ImageField(blank=True)
+    source = models.URLField(blank=True)
+    group = models.ForeignKey(DocumentationGroup, on_delete=models.CASCADE)
