4d3c0d98590e8956888bb9462a67a3233e90f3fc
Asnel Christian <ngoullasob@gmail.com>
2018-07-01 15:04:58 +0100

    Tests js functions

---
+++ data_Insights_website/static/js/test/stats/DispersionTest.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var Dispertion = require('../../modules/stats/dispersion')
+
+describe('Dispersion()', function() {
+    it('', function() {
+        assert.deepEqual()
+    });
+
+    it('', function() {
+        assert.deepEqual()
+    });
+
+    it('', function() {
+        assert.deepEqual();
+    });
+})
