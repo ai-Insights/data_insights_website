fee1da38cbdd01a1f13b9051e582d8ce70bb7dd5
Asnel Christian <ngoullasob@gmail.com>
2018-07-03 10:49:35 +0100

    Add populate_db and clear_db for docs app

---
+++ data_Insights_website/docs/management/commands/clear_db.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from data_Insights_website.docs.models import *
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Clear docs db'
+    def handle(seld, *args, **options):
+        DocumentationGroup.objects.all().delete()
+        DocumentationEntryStat.objects.all().delete()
